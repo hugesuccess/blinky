1f5275105b7875c5b8107455ffa157456ae021ba
Victor Venters <victor@huge-success.net>
2014-10-09 01:08:59 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -1,71 +1,47 @@
 var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
-var j5 = require("johnny-five");
-var board, leds=[], ledPins = [2,3,4,5,6,7,8,9];
-board = new j5.Board();
+var five = require("johnny-five"),
+    board, myMotor, led;
 
+board = new five.Board();
 server.listen(3000);
 
 app.get('/', function (req, res) {
       res.sendfile(__dirname + '/index.html');
 });
 
+
 board.on("ready", function() {
+  board.activated = true;
+  myMotor = new five.Motor({
+    pin: 9
+  });
+  // event handlers on start and stop
+  myMotor.on("start", function( err, timestamp ) {
+    console.log( "started", timestamp );
+  }); 
+  myMotor.on("stop", function( err, timestamp ) {
+    console.log( "stopped", timestamp );
+  });
 
-  // initialize LEDs using a for loop
-  for (var i = 0; i < ledPins.length; i++){
-      var myLed = new j5.Led(ledPins[i]);
-      leds.push(myLed);
-  }     
+  myMotor.start();
+  // stop after 2 seconds
+  board.wait(2000, function() {
+    myMotor.stop();
+  });
 
-   allOn();
-  // board.wait(1000,allOff);
-  //oneAfterAnother();
-  //board.loop(4500, oneAfterAnother);
 });
 
 io.on('connection', function (socket) {
-       socket.on('state', function (data) {
-        if(data.state === "all") {
-            allOff();
-            allOn();
-        }                 
+        socket.on('state', function (data) {
+            //j5 Check
+            if(board && !board.activated) return false;
 
-        if(data.state === "off") {
-            allOff();
-        }                 
-        if(data.state === "blink") {
-            allOff();
-            board.loop(4500, oneAfterAnother);
-        }                 
-       });
+            if(data.motor)
+                return myMotor.start();
+
+            return myMotor.stop();
+            
+        });
 });
-
-  function allOn(){
-    for (var i = 0; i < leds.length; i++) {
-        leds[i].on();
-    }
-  } 
-  function allOff(){
-    for (var i = 0; i < leds.length; i++) {
-        leds[i].off();
-    }
-  }
-  function oneAfterAnother() {
-      var delay = 1;
-      board.counter = 0;
-      for (var i = 0; i < leds.length; i++) {
-        var led = leds[i];
-        board.wait(delay,function(){
-            console.log(this.counter + " on");
-            leds[this.counter].on();
-        });
-        board.wait(delay + 200,function(){
-            console.log(this.counter + " off");
-            leds[this.counter].off();
-            this.counter = (this.counter + 1) % leds.length;
-        });
-        delay += 500;
-      }
-  }
