2653f8f823222342db8b2b78aecd5b9e80afe79f
Victor Venters <victor@huge-success.net>
2014-10-08 19:02:48 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -2,11 +2,8 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var j5 = require("johnny-five");
-var board, blinky;
-
+var board, leds=[], ledPins = [2,3,4,5,6,7,8,9];
 board = new j5.Board();
-board.actived = false;
-
 
 server.listen(3000);
 
@@ -14,27 +11,61 @@ app.get('/', function (req, res) {
       res.sendfile(__dirname + '/index.html');
 });
 
-/*
- *  Websocket Connection  
- *    client sends a an object w/ led param with a boolean value
- *    {led: BOOL}
- *    If true
- */
+board.on("ready", function() {
+
+  // initialize LEDs using a for loop
+  for (var i = 0; i < ledPins.length; i++){
+      var myLed = new j5.Led(ledPins[i]);
+      leds.push(myLed);
+  }     
+
+   allOn();
+  // board.wait(1000,allOff);
+  //oneAfterAnother();
+  //board.loop(4500, oneAfterAnother);
+});
 
 io.on('connection', function (socket) {
-        socket.on('state', function (data) {
-            //j5 Check
-            if(board && !board.activated) return false;
+       socket.on('state', function (data) {
+        if(data.state === "all") {
+            allOff();
+            allOn();
+        }                 
 
-            if(data.led)
-                return blinky.on();
-        
-            return blinky.off();
+        if(data.state === "off") {
+            allOff();
+        }                 
+        if(data.state === "blink") {
+            allOff();
+            board.loop(4500, oneAfterAnother);
+        }                 
+       });
+});
+
+  function allOn(){
+    for (var i = 0; i < leds.length; i++) {
+        leds[i].on();
+    }
+  } 
+  function allOff(){
+    for (var i = 0; i < leds.length; i++) {
+        leds[i].off();
+    }
+  }
+  function oneAfterAnother() {
+      var delay = 1;
+      board.counter = 0;
+      for (var i = 0; i < leds.length; i++) {
+        var led = leds[i];
+        board.wait(delay,function(){
+            console.log(this.counter + " on");
+            leds[this.counter].on();
         });
-});
-
-board.on("ready", function() {
-  board.activated = true;
-  blinky = new j5.Led(13);
-});
-
+        board.wait(delay + 200,function(){
+            console.log(this.counter + " off");
+            leds[this.counter].off();
+            this.counter = (this.counter + 1) % leds.length;
+        });
+        delay += 500;
+      }
+  }
