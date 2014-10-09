670f01df36528c64703ec62a2bf232c0f9578e2a
Victor Venters <victor@huge-success.net>
2014-10-09 02:11:52 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -2,37 +2,35 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var five = require("johnny-five"),
-board, myServo;
+    board, shiftRegister, value;
 
 board = new five.Board();
 server.listen(3000);
 
-app.get('/', function (req, res) {
-      res.sendfile(__dirname + '/index.html');
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
 });
 
 board.on("ready", function() {
-  board.activated = true;
-  myServo = new five.Servo(9);
+  shiftRegister = new five.ShiftRegister({
+    pins: {
+      data: 2,
+      clock: 3,
+      latch: 4
+    }
+  });
+  value = 0;
 
-  myServo.sweep();
-
-  this.wait(5000, function(){
-    myServo.stop();
-    myServo.center();
-  });  
 });
 
-io.on('connection', function (socket) {
-    socket.on('state', function (data) {
-        //j5 Check
-        if(board && !board.activated) return false;
-
-        if(data.servo)
-            return myServo.sweep();
-    
-        return myServo.stop();
+io.on('connection', function(socket) {
+    socket.on('state', function(data) {
+      next();
     });
 });
 
-
+function next() {
+    value = value > 0x11 ? value >> 1 : 0x88;
+    shiftRegister.send( value );
+    setTimeout(next, 300);
+}
