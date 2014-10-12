5a4de31b053c6d222508c74a46311167546b4814
Victor Venters <victor@huge-success.net>
2014-10-12 02:33:27 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -2,35 +2,35 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var five = require("johnny-five"),
-    board, shiftRegister, value;
+    onButton, offButton, led, sock = false;
 
-board = new five.Board();
 server.listen(3000);
 
-app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
+app.get('/', function (req, res) {
+  res.sendfile(__dirname + '/index.html');
 });
 
-board.on("ready", function() {
-  shiftRegister = new five.ShiftRegister({
-    pins: {
-      data: 2,
-      clock: 3,
-      latch: 4
-    }
-  });
-  value = 0;
-
-});
-
-io.on('connection', function(socket) {
-    socket.on('state', function(data) {
-      next();
+five.Board().on("ready", function() {
+    onButton = new five.Button({
+        pin: 2
     });
+    offButton = new five.Button({
+        pin: 3
+    });
+
+  led = new five.Led(13);
+  onButton.on("down", function(value){
+       led.on();
+       if(sock) sock.emit('state', { led: 1 });
+  });
+
+  offButton.on("down", function(){
+      led.off();
+      if(sock) sock.emit('state', { led: 0 });
+  });
 });
 
-function next() {
-    value = value > 0x11 ? value >> 1 : 0x88;
-    shiftRegister.send( value );
-    setTimeout(next, 300);
-}
+
+io.on('connection', function (socket) {
+    sock = socket;
+});
