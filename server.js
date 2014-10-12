a65deb0de8cc064dfe131a449b9fc54fb5a7ddb2
Victor Venters <victor@huge-success.net>
2014-10-12 04:44:42 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -2,28 +2,29 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var five = require("johnny-five"),
-    board, myPotentiometer, threshold = 512, sock = false, percentage = 0;
-board = new five.Board();
+    board, myPhotoresistor, myLed, sock = false;
 
+board = new five.Board();
 server.listen(3000);
 
 app.get('/', function(req, res) {
-    res.sendfile(__dirname + '/index.html');
-})
+    res.sendfile(__dirname + "/index.html");
+});
 
 board.on("ready", function() {
-  myPotentiometer = new five.Sensor({
+  myLed = new five.Led(9);
+  myPhotoresistor = new five.Sensor({
     pin: "A0",
     freq: 250
   });
-  myLed = new five.Led(9);
-  myPotentiometer.on("read", function() {
-    percentage = (this.raw/1023) * 100;
-    if(sock) sock.emit('state', {led: percentage});
-    if (this.raw > threshold) {
-       myLed.on();
+  myPhotoresistor.on("read", function( err, value ) {
+    var threshold = 300;
+    if (value > threshold) {
+        myLed.on();
+        if(sock) sock.emit('state', {on: 1});
     } else {
-       myLed.off();   
+        myLed.off();
+        if(sock) sock.emit('state', {on: 0});
     }
   });
 });
