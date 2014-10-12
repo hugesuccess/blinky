4674408dfe98774e502205a80227bf560135b93b
Victor Venters <victor@huge-success.net>
2014-10-12 04:26:14 -0700

    Initial Commit

--- server.js
+++ server.js
@@ -2,35 +2,32 @@ var app = require('express')();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
 var five = require("johnny-five"),
-    onButton, offButton, led, sock = false;
+    board, myPotentiometer, threshold = 512, sock = false, percentage = 0;
+board = new five.Board();
 
 server.listen(3000);
 
-app.get('/', function (req, res) {
-  res.sendfile(__dirname + '/index.html');
-});
+app.get('/', function(req, res) {
+    res.sendfile(__dirname + '/index.html');
+})
 
-five.Board().on("ready", function() {
-    onButton = new five.Button({
-        pin: 2
-    });
-    offButton = new five.Button({
-        pin: 3
-    });
-
-  led = new five.Led(13);
-  onButton.on("down", function(value){
-       led.on();
-       if(sock) sock.emit('state', { led: 1 });
+board.on("ready", function() {
+  myPotentiometer = new five.Sensor({
+    pin: "A0",
+    freq: 250
   });
-
-  offButton.on("down", function(){
-      led.off();
-      if(sock) sock.emit('state', { led: 0 });
+  myLed = new five.Led(9);
+  myPotentiometer.on("read", function() {
+    percentage = (this.raw/1023) * 100;
+    if(sock) sock.emit('state', {led: percentage});
+    if (this.raw > threshold) {
+       myLed.on();
+    } else {
+       myLed.off();   
+    }
   });
 });
 
-
-io.on('connection', function (socket) {
+io.on('connection', function(socket) {
     sock = socket;
 });
